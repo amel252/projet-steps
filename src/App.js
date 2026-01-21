ad677048f89f5d4a53b0bb0fc8f55b4a0648e68e
Amel252 <amelghanem25@gmail.com>
2026-01-22 00:37:08 +0100

    Création de notre App code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./index.css";
 
-function App() {
-    return <div>Hello react</div>;
-}
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
 
-export default App;
+export default function App() {
+    const step = 1;
+
+    return (
+        <div className="steps">
+            <div className="numbers">
+                {/*  si le step est inf a 1 active l'option active */}
+                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+            </div>
+            <p className="message">
+                Step {step}:{messages[step - 1]}
+            </p>
+            <div className="buttons">
+                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+                    Previous
+                </button>
+                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
