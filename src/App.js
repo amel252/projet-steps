999a782ac0e1346ce7f726bbdaf0c3d5f59f407e
Amel252 <amelghanem25@gmail.com>
2026-02-10 01:18:19 +0100

    Création de Steps component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,11 @@
 import { useState } from "react";
 import "./index.css";
-
-const messages = [
-    "Learn React ⚛️",
-    "Apply for jobs 💼",
-    "Invest your new income 🤑",
-];
+import Steps from "./components/Steps";
 
 export default function App() {
-    const [step, setStep] = useState(1);
-    const [isOpen, setIsOpen] = useState(true);
-
-    function handlePrevious() {
-        //  fonction pour arreter a 1, s est toujours la valeur la plus récente du state
-        if (step > 1) setStep((s) => s - 1);
-    }
-    function handleNext() {
-        //  fonction pour arreter à 3
-        if (step < 3) setStep((s) => s + 1);
-    }
-
     return (
         <>
-            <button className="close" onClick={() => setIsOpen((is) => !is)}>
-                &times;
-            </button>
-            {isOpen && (
-                <div className="steps">
-                    <div className="numbers">
-                        {/*  si le step est inf a 1 active l'option active */}
-                        <div className={step >= 1 ? "active" : ""}>1</div>
-                        <div className={step >= 2 ? "active" : ""}>2</div>
-                        <div className={step >= 3 ? "active" : ""}>3</div>
-                    </div>
-                    <p className="message">
-                        Step {step}:{messages[step - 1]}
-                    </p>
-                    <div className="buttons">
-                        <button
-                            style={{
-                                backgroundColor: "#7950f2",
-                                color: "#fff",
-                            }}
-                            onClick={handlePrevious}
-                        >
-                            Previous
-                        </button>
-                        <button
-                            style={{
-                                backgroundColor: "#7950f2",
-                                color: "#fff",
-                            }}
-                            onClick={handleNext}
-                        >
-                            Next
-                        </button>
-                    </div>
-                </div>
-            )}
+            <Steps />
         </>
     );
 }
